dce621009e6ed1cdb371fb90d7e9a3392028e5ca
Adrian Constantin Vaideanu <vaideanu.adrian@gmail.com>
2022-12-20 11:44:59 +0200

    Define level service to allow for user customization

---
+++ RewardMatic 4000/RewardMatic 4000/Service/LevelService.cs
@@ -0,0 +1,28 @@
+﻿using RewardMatic_4000.Infrastructure;
+using RewardMatic_4000.Model;
+using System.Collections.Generic;
+
+namespace RewardMatic_4000.Service
+{
+    public class LevelService : ILevelService
+    {
+        private IRewardsReader _reader;
+        private User _user;
+
+        public LevelService(IRewardsReader reader)
+        {
+            _reader = reader;
+        }
+
+        public User GetUser()
+        {
+            if (_user == null)
+            {
+                IList<RewardGroup> groups = _reader.Read();
+                _user = new User(groups);
+            }
+
+            return _user;
+        }
+    }
+}
